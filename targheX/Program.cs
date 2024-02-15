b6f46b86393967b0af92b409aa094a095d65199b
MIRCO MASSACCESI <mirco.massaccesi@studenti.unicam.it>
2024-02-15 11:37:16 +0100

    Inserimento area identity e backed

--- targheX/Program.cs
+++ targheX/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using targheX.Data;
+using Microsoft.AspNetCore.Identity;
+using targheX.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,10 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<DbContextUser>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<DbContextUser>();
 
 
 var app = builder.Build();
@@ -27,4 +33,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapRazorPages();
 app.Run();
